23a455f6982eefda671904a9d63b045980c9999a
hemant <hemantarora091@gmail.com>
2024-02-19 10:04:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import SocketState from "./context/SocketState"
+import Room from './components/Room';
+import PeerState from './context/PeerState';
+import Home2 from './components/Home2';
+import Home3 from './components/Home3';
+import Home from './components/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <div>
+      <SocketState>
+        <PeerState>
+         <Router>
+
+          <Routes>
+            <Route path="/home" element={<Home/>} />
+            <Route path="/" element={<Home2/>} />
+            <Route path="/room/:roomId" element={<Room />} />
+            <Route path="/signup" element={<Home3 />} />
+
+          </Routes>
+
+        </Router>
+        </PeerState>
+      </SocketState>
+      </div>
+    </>
   );
 }
 
